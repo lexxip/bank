ae9d98a854941b96a65017144bcd1f519f6d0b52
Алексей Ширяев <lexxip@yandex.ru>
2023-10-15 14:30:46 +0500

    Дополнение

--- profile/src/main/java/com/bank/profile/ProfileApplication.java
+++ profile/src/main/java/com/bank/profile/ProfileApplication.java
@@ -2,7 +2,9 @@ package com.bank.profile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ProfileApplication {
     public static void main(String[] args) {
